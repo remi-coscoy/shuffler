c16951be16fad21d981c4b426496dbf74ccae0a5
Rémi Coscoy <remi.coscoy@protonmail.com>
2025-03-14 22:31:56 +0100

    fix: moved shuffles methods to their own file, feat(stats, main): added first statistics module

--- src/shuffler/main.py
+++ src/shuffler/main.py
@@ -1,5 +1,29 @@
+from deck import StandardDeck
+from shuffles import (
+    random_shuffle,
+    fisher_yates_shuffle,
+    overhand_shuffle,
+    riffle_shuffle,
+)
+from deck_statistics import stats_main
+
+
 def main():
-    print("Hello from shuffler!")
+    deck = StandardDeck()
+    for num_shuffle in [1, 10, 100]:
+        print(f"**{num_shuffle} shuffles**")
+        for shuffle_method in [
+            random_shuffle,
+            fisher_yates_shuffle,
+            overhand_shuffle,
+            riffle_shuffle,
+        ]:
+            result = stats_main(
+                deck=deck, shuffle_method=shuffle_method, number_of_shuffles=num_shuffle
+            )
+            print(
+                f"The average distance from starting position with the {shuffle_method.__name__} method and {num_shuffle} shuffles is {result:.2f}"
+            )
 
 
 if __name__ == "__main__":
